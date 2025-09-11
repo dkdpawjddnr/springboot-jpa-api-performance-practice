435b5a686569682880dd3cdb6154c45cb1681af2
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-09-12 02:18:44 +0900

    feat: 회원 조회 API

--- jpashop-web1-inf/src/main/java/jpabook_web1_inf/jpashop_web1_inf/api/MemberApiController.java
+++ jpashop-web1-inf/src/main/java/jpabook_web1_inf/jpashop_web1_inf/api/MemberApiController.java
@@ -9,11 +9,43 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    // v1의 문제점 엔타타의 모든 값이 노출이 됨.
+    // 엔티티가 변경되면 API 스펙이 변한다.
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
 
     // 엔티티를 RequestBody에 직접 매핑
     @PostMapping("/api/v1/members")
