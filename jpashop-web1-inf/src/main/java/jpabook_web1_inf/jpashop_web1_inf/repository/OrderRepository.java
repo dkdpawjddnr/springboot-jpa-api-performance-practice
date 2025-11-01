00bf360cbd77a7159f95bd80e594a331c38f4b05
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-11-01 21:44:03 +0900

    주문 조회 V3.1: 엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- jpashop-web1-inf/src/main/java/jpabook_web1_inf/jpashop_web1_inf/repository/OrderRepository.java
+++ jpashop-web1-inf/src/main/java/jpabook_web1_inf/jpashop_web1_inf/repository/OrderRepository.java
@@ -89,12 +89,24 @@ public class OrderRepository {
         return query.getResultList();
     }
 
-    // Fetch Join으로 LAZY 무시하고 진짜 객체를 가져온다.
     public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                        "select o from Order o" +
+                           " join fetch o.member m" +
+                           " join fetch o.delivery d", Order.class).getResultList();
+
+    }
+
+    // Fetch Join으로 LAZY 무시하고 진짜 객체를 가져온다.
+    // ToOne 관계라 한방 쿼리
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
         return em.createQuery(
                 "select o from Order o" +
                 " join fetch o.member m" +
-                " join fetch o.delivery d", Order.class).getResultList();
+                " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
     }
 
     public List<Order> findAllWithItem() {
